d7c09774d7999a580a3fd9e0834837ee6a1f8e9d
denalfek2 <denalfek2@gmail.com>
2020-06-24 15:19:37 +0300

    - add controller - add service

---
+++ MultiThreadingLongReading/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiThreadingLongReading.Services;
+
+namespace MultiThreadingLongReading.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [Route("withoutlock")]
+        public IActionResult WithoutLock()
+        {
+            var res = ReaderService.ReadFile();
+            return Ok(res);
+        }
+
+        [Route("withlock")]
+        public IActionResult WithLock()
+        {
+            var res = ReaderService.ReadFileWithLock();
+            return Ok(res);
+        }
+
+        [Route("monitor")]
+        public IActionResult Monitor()
+        {
+            var res = ReaderService.MonitorTest();
+            return Ok(res);
+        }
+    }
+}
---
+++ MultiThreadingLongReading/Services/ReaderService.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading;
+
+namespace MultiThreadingLongReading.Services
+{
+    public static class ReaderService
+    {
+        private static string _file;
+        private static object _locker;
+
+        public static string ReadFile()
+        {
+            Thread.Sleep(20000);
+            return $"Hello from thread {Thread.CurrentThread.ManagedThreadId}";
+        }
+
+        public static string ReadFileWithLock()
+        {
+            // Вернуть в оба потока надо, посмотреть про именованный семафор, zookeeper, webdav
+            if (Monitor.TryEnter(_locker))
+            {
+                try
+                {
+                    _file = $"Hello from thread {Thread.CurrentThread.ManagedThreadId}";
+                    Thread.Sleep(20000);
+                    return _file;
+                }
+                finally
+                {
+                    Monitor.Exit(_locker);
+                }
+            }
+            else
+            {
+                Monitor.Wait(_locker); // todo: async wait
+                return _file;
+            }
+        }
+
+        public static string MonitorTest()
+        {
+            object locker = new object();
+            bool acquiredLock = false;
+            try
+            {
+                Monitor.Enter(locker, ref acquiredLock);
+                Thread.Sleep(20000);
+                return $"Hello from thread {Thread.CurrentThread.ManagedThreadId}";
+            }
+            finally
+            {
+                if (acquiredLock)
+                {
+                    Monitor.Exit(locker);
+                }
+            }
+        }
+    }
+}
